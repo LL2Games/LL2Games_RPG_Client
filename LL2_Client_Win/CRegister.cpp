9f5e0da9f4228b4652099bad682c577d1d85d03c
gunoo22 <gunoo22@ksign.com>
2026-08-11 10:46:45 +0900

    feat: 비밀번호 정책 추가, 다이얼로그에 정책표시 - 9~50자리 - 대문자/소문자/숫자/특수문자 중 3종류 이상 - 특수문자 필수 - 동일문자 3연속 금지 - 연속문자 3자리 금지

--- LL2_Client_Win/CRegister.cpp
+++ LL2_Client_Win/CRegister.cpp
@@ -45,6 +45,136 @@ static BOOL CheckIdValidate(const CString& strName, CString& strErrMsg)
 	return TRUE;
 }
 
+static BOOL CheckPwdValidate(const CString& strPwd, CString& strErrMsg, BOOL bComplexity = FALSE)
+{
+	if (strPwd.IsEmpty())
+	{
+		strErrMsg = _T("비밀번호를 입력하세요.");
+		return FALSE;
+	}
+
+	// bComplexity == TRUE이면 복잡도 검사 생략
+	if (bComplexity)
+	{
+		return TRUE;
+	}
+
+	const int nPassLen = strPwd.GetLength();
+
+	// 길이 제한
+	if (nPassLen < 9 || nPassLen > 50)
+	{
+		strErrMsg = _T("비밀번호는 최소 9자리 이상 50자리 이하여야 합니다.");
+		return FALSE;
+	}
+
+	BOOL bAlphaUP = FALSE;	// 영문 대문자
+	BOOL bAlphaLO = FALSE;	// 영문 소문자
+	BOOL bDigit = FALSE;	// 숫자
+	BOOL bSpecial = FALSE;	// 특수문자
+
+	int nSame = 0;			// 동일 문자 연속 횟수
+	int nASC = 0;			// 연속 문자 오름차순
+	int nDESC = 0;			// 연속 문자 내림차순
+
+	for (int i = 0; i < nPassLen; ++i)
+	{
+		TCHAR ch = strPwd[i];
+
+		// 문자 종류 확인
+		if (ch >= _T('A') && ch <= _T('Z'))
+		{
+			bAlphaUP = TRUE;
+		}
+		else if (ch >= _T('a') && ch <= _T('z'))
+		{
+			bAlphaLO = TRUE;
+		}
+		else if (ch >= _T('0') && ch <= _T('9'))
+		{
+			bDigit = TRUE;
+		}
+		else
+		{
+			bSpecial = TRUE;
+		}
+
+		// 같은 문자 3번 이상 연속 사용 금지
+		if (i > 0 && strPwd[i] == strPwd[i - 1])
+		{
+			nSame++;
+
+			if (nSame > 1)
+			{
+				strErrMsg = _T("같은 문자를 3자리 이상 연속으로 사용할 수 없습니다.");
+				return FALSE;
+			}
+		}
+		else
+		{
+			nSame = 0;
+		}
+
+		// 연속 문자 3자리 이상 금지
+		if (i > 0)
+		{
+			int nDiff = strPwd[i] - strPwd[i - 1];
+
+			if (nDiff == 1)
+			{
+				nASC++;
+				nDESC = 0;
+			}
+			else if (nDiff == -1)
+			{
+				nDESC++;
+				nASC = 0;
+			}
+			else
+			{
+				nASC = 0;
+				nDESC = 0;
+			}
+
+			if (nASC > 1 || nDESC > 1)
+			{
+				strErrMsg = _T("연속된 문자를 3자리 이상 사용할 수 없습니다.");
+				return FALSE;
+			}
+		}
+	}
+
+	int nVariable = 0;
+
+	if (bAlphaUP)
+		nVariable++;
+
+	if (bAlphaLO)
+		nVariable++;
+
+	if (bDigit)
+		nVariable++;
+
+	if (bSpecial)
+		nVariable++;
+
+	// 특수문자 필수
+	if (!bSpecial)
+	{
+		strErrMsg = _T("비밀번호에는 특수문자가 1개 이상 포함되어야 합니다.");
+		return FALSE;
+	}
+
+	// 대문자 / 소문자 / 숫자 / 특수문자 중 3종류 이상
+	if (nVariable < 3)
+	{
+		strErrMsg = _T("비밀번호는 영문 대문자, 영문 소문자, 숫자, 특수문자 중 3종류 이상을 조합해야 합니다.");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
 
 // CRegister 대화 상자
 
@@ -167,17 +297,18 @@ void CRegister::OnBnClickedButtonRegister()
 	}
 
 	//비밀번호 입력값 예외처리
-	if (strPw.GetLength() == 0)
-	{
-		AfxMessageBox(_T("비밀번호를 입력하세요"));
-		return;
-	}
-
 	if (strPw != strPwChecked)
 	{
 		AfxMessageBox(_T("비밀번호 입력값이 다릅니다."));
 		return;
 	}
+
+	if (!CheckPwdValidate(strPw, strErrMsg))
+	{
+		AfxMessageBox(strErrMsg);
+		return;
+	}
+
 #endif
 
 	Register(strID, strPw, strPwChecked);
